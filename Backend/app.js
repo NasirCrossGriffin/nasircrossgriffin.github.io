48cafd943aa731d79191a59896142e2106c46b8b
NasirCrossGriffin <nasircrossgriffin@gmail.com>
2024-07-18 16:48:08 -0400

    Dont give up challenge again

--- Backend/app.js
+++ Backend/app.js
@@ -47,8 +47,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use("/api/tasks", tasksRoutes); 
 app.use("/api/users", usersRoutes);
-
-app.use(express.static(path.join(__dirname, "..", "Frontend", "build")));
+app.use(express.static(path.join(__dirname, '../Frontend/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "..", "Frontend", "build", 'index.html'), err => {
